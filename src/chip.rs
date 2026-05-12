3d860fe173cccefe357146bf89b03d8954b6c0de
Stephen Lynch <stepheng.lynch@gmail.com>
2026-05-12 22:22:18 +0100

    Pulse channel works much better now!

--- src/chip.rs
+++ src/chip.rs
@@ -164,11 +164,11 @@ impl Pulse {
                 // Do nothing
             },
             2 => {
-                self.timer &= 0x00ff;
+                self.timer &= !0x00ff;
                 self.timer |= val as u16;
             },
             3 => {
-                self.timer &= 0x0f00;
+                self.timer &= !0x0f00;
                 self.timer |= (val as u16 & 0x07) << 8;
                 self.length = LENGTH_TABLE[(val >> 3) as usize];
                 self.envelope.set_start();
